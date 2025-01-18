63fc5aca27dfb46ac688afad75d7d78f8fb07447
Jakub Wypych <01178898@pw.edu.pl>
2025-01-18 21:29:12 +0100

    CSRF + turn off Server + CSP

--- app/app.py
+++ app/app.py
@@ -1,11 +1,25 @@
 # app.py
-from flask import Flask
+from flask import Flask, Talisman
 from flask_login import LoginManager
 from routes import setup_auth_routes, setup_notes_routes, setup_register_routes
 from database import init_db
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.secret_key = "206363ef77d567cc511df5098695d2b85058952afd5e2b1eecd5aed981805e60"
+
+app.config['SERVER_NAME'] = 'localhost'
+app.after_request(lambda response: response.headers.pop('Server', None) or response)
+
+# Konfiguracja CSP
+csp = {
+    'default-src': ["'self'"],
+    'script-src': ["'self'"],
+    'style-src': ["'self'"]
+}
+
+Talisman(app, content_security_policy=csp)
+csrf = CSRFProtect(app)
 
 # Login manager setup
 login_manager = LoginManager()
